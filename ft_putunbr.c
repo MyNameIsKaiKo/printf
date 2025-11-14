dcbe6c7c0a50035d036c25cc9ceb37fa1a0c2a7a
Jean-Baptiste Leray <jleray@3C5.42angouleme.fr>
2025-11-14 18:51:22 +0100

    add nbcar to my putunbr

--- ft_putunbr.c
+++ ft_putunbr.c
@@ -12,16 +12,34 @@
 
 #include "printf.h"
 
-void	ft_putunbr(unsigned int nb)
+int	nbcar_calc(int nb)
 {
+	int	i;
+
+	i = 0;
+	while (nb > 0)
+	{
+		nb = nb / 10;
+		i++;
+	}
+	return (i);
+}
+
+int	ft_putunbr(unsigned int nb)
+{
+	int	i;
+
+	i = 0;
 	if (nb < 10)
 	{
 		ft_putchar_fd(nb + '0', 1);
-		return ;
+		i++;
+		return (i);
 	}
 	else
 	{
 		ft_putunbr(nb / 10);
 		ft_putunbr(nb % 10);
 	}
+	return (nbcar_calc(nb));
 }
