7776132505beb0f5b8c1f46721ddc7bd76ed9d76
Jean-Baptiste Leray <jleray@3A6.42angouleme.fr>
2025-11-15 20:50:43 +0100

    added things to do home

--- ft_printf.c
+++ ft_printf.c
@@ -15,7 +15,22 @@
 int	ft_printf(const char *str, ...)
 {
 	va_list	args;
+	size_t	i;
 
+	i = 0;
 	va_start(args, str);
+	while (*str)
+	{
+		if (*str == '%')
+		{
+			str++;
+			//i = print_tag(str, args);
+		}
+		else
+		{
+			i++;
+			ft_putchar_fd(*str, 1);
+		}
+	}
 	return (0);
 }
